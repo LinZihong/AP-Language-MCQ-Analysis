f024a791e49404fb29b726d34de26d0f90469b25
ZihongLin <1046298030@qq.com>
2018-04-28 15:24:18 +0800

    make distill into a function

--- distill.py
+++ distill.py
@@ -1,14 +1,15 @@
 import re
 
-regexStr = r"(?s)\(A\).*?(?=[0-9]|Unauthorized|Commercial)"
+def distill():
+    regexStr = r"(?s)\(A\).*?(?=[0-9]|Unauthorized|Commercial)"
+    regex = re.compile(regexStr)
+    text = open('./pdf/AP Lang 2015.txt', 'r', errors='ignore').read()
+    escape = ["\"", "\'", ",", ".", "!", "?", "(A)", "(B)", "(C)", "(D)", "(E)"]
+    regexFound = regex.findall(text)
 
-regex = re.compile(regexStr)
+    for i in range(len(regexFound)):
+        for char in escape:
+            regexFound[i] = regexFound[i].replace(char, "")
+            # print(result)
 
-text = open('./pdf/AP Lang 2015.txt', 'r', errors='ignore').read()
-
-escape = ["\"", "\'", ",", ".", "!", "?", "(A)", "(B)", "(C)", "(D)", "(E)"]
-
-for result in regex.findall(text):
-    for char in escape:
-        result = result.replace(char, "")
-    print(result)
+    return regexFound
