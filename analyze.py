b8edeeb3ceb036edbabac647289ea40081853ff7
ZihongLin <1046298030@qq.com>
2018-04-28 15:40:44 +0800

    finish load_word_list()

--- analyze.py
+++ analyze.py
@@ -1,3 +1,15 @@
 from distill import *
+# from word_forms.word_forms import get_word_forms
 
-print(distill())
+def get_flattened_word_forms(word):
+    wfs = get_word_forms(word)
+    merged = set()
+    for k, s in wfs.items():
+        merged = merged.union(s)
+    return merged
+
+def load_word_list(filename):
+    text = open(filename,'r',errors='ignore').readlines()
+    return set([x.strip() for x in text])
+
+print(load_word_list('Barron_simplified.txt'))
